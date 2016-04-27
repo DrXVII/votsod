e909ca88715b3655ad831378341d5bc6d9ea1c32
cofogo <egined@gmail.com>
2016-04-27 21:00:32 +0300

    more code refactored

--- src/Char_Human.cpp
+++ src/Char_Human.cpp
@@ -1,23 +1,34 @@
 #include "Char_Human.h"
 
-Char_Human::Char_Human(int _x, int _y): Char(_x, _y){
+Char_Human::Char_Human(int _x, int _y): Char(_x, _y)
+{
 	m_icon = '@';
 }
 
-Char_Human::~Char_Human(){
+Char_Human::~Char_Human()
+{
 	//dtor
 }
 
-int Char_Human::take_turn(){
+int Char_Human::take_turn()
+{
 	int cmd = ' ';
 	do{
 		cmd = getch();
 		switch(cmd){
-		case KEY_LEFT : /*if(_M.GetTile(Y, X-1).GetPassable())*/ m_x -= 1; return cmd;
-		case KEY_RIGHT: /*if(_M.GetTile(Y, X+1).GetPassable())*/ m_x += 1; return cmd;
-		case KEY_UP	 : /*if(_M.GetTile(Y-1, X).GetPassable())*/ m_y -= 1; return cmd;
-		case KEY_DOWN : /*if(_M.GetTile(Y+1, X).GetPassable())*/ m_y += 1; return cmd;
-		case 'q': return cmd;
+			case KEY_LEFT:
+				/*if(_M.GetTile(Y, X-1).GetPassable())*/ m_x -= 1;
+				return cmd;
+			case KEY_RIGHT:
+				/*if(_M.GetTile(Y, X+1).GetPassable())*/ m_x += 1;
+				return cmd;
+			case KEY_UP:
+				/*if(_M.GetTile(Y-1, X).GetPassable())*/ m_y -= 1;
+				return cmd;
+			case KEY_DOWN:
+				/*if(_M.GetTile(Y+1, X).GetPassable())*/ m_y += 1;
+				return cmd;
+			case 'q': return cmd;
 		}
 	}while(cmd != 'q');
 	return cmd;
