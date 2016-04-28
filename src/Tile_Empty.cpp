61961cb01c8f3f85c0e3cb7937baac47b3de56f8
cofogo <egined@gmail.com>
2016-04-28 15:30:43 +0300

    refactored code layout

--- src/Tile_Empty.cpp
+++ src/Tile_Empty.cpp
@@ -1,11 +1,9 @@
 #include "Tile_Empty.h"
 
-Tile_Empty::Tile_Empty(){
+Tile_Empty::Tile_Empty()
+{
 	SetIcon('.');
 	SetPassable(true);
 }
 
-Tile_Empty::~Tile_Empty()
-{
-	//dtor
-}
+Tile_Empty::~Tile_Empty(){}
