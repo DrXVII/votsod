3a2b6cfa3e1df1f41960dcee64302a9d48defb61
cofogo <egined@gmail.com>
2016-04-28 15:37:03 +0300

    refactored code

--- include/Char_Human.h
+++ include/Char_Human.h
@@ -4,12 +4,13 @@
 #include <Char.h>
 
 
-class Char_Human : public Char{
-	public:
-		Char_Human(int _x, int _y);
-		virtual ~Char_Human();
+class Char_Human : public Char
+{
+public:
+	Char_Human(int _x, int _y);
+	virtual ~Char_Human();
 
-		int take_turn();
+	int take_turn();
 };
 
 #endif // CHAR_HUMAN_H
