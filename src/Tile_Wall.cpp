c4dd64ad85672e7c485a59adb5f501305189c245
cofogo <egined@gmail.com>
2016-04-28 15:31:18 +0300

    refactored code layout

--- src/Tile_Wall.cpp
+++ src/Tile_Wall.cpp
@@ -1,10 +1,9 @@
 #include "Tile_Wall.h"
 
-Tile_Wall::Tile_Wall(){
+Tile_Wall::Tile_Wall()
+{
 	SetIcon('#');
 	SetPassable(false);
 }
 
-Tile_Wall::~Tile_Wall(){
-	//dtor
-}
+Tile_Wall::~Tile_Wall(){}
